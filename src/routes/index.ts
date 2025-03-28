ab799ec3bb363a5da8d945baf95bc91fd284c584
apucsd <apusutradhar77@gmail.com>
2025-03-28 11:13:39 +0600

    added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { AuthRoutes } from '../app/modules/auth/auth.route';
 import { UserRoutes } from '../app/modules/user/user.route';
+import { FAQRoutes } from '../app/modules/faqs/faq.route';
+import { PrivacyPolicyRoutes } from '../app/modules/privacy-policy/pp.route';
+import { TermsAndConditionsRoutes } from '../app/modules/terms-and-conditions/tc.route';
 
 const router = express.Router();
 
@@ -12,6 +15,18 @@ const apiRoutes = [
       {
             path: '/auth',
             route: AuthRoutes,
+      },
+      {
+            path: '/faqs',
+            route: FAQRoutes,
+      },
+      {
+            path: '/privacy-policy',
+            route: PrivacyPolicyRoutes,
+      },
+      {
+            path: '/terms-and-conditions',
+            route: TermsAndConditionsRoutes,
       },
 ];
 
